bc91eeb9297841337a994e9744c74cccb8001256
ambarmishraa <ambarmishra740@gmail.com>
2024-03-14 00:05:08 +0530

    get completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, docData, getDocs, setDoc} from '@angular/fire/firestore';
+import { collection, collectionData} from '@angular/fire/firestore';
+import { doc,addDoc,getDoc, Timestamp, updateDoc, serverTimestamp} from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'firebaseAngular';
-}
+
+   firestore = inject(Firestore);
+  
+   //adding data in firebase
+  //  name=""
+  //  email=""
+  // async submit(){
+  //   const check= await addDoc(collection(this.firestore, "cities"), {
+  //     name: this.name,
+  //     email: this.email
+  //   }  
+  //   );
+  async ngOnInit(): Promise<void> {
+    // try {
+    //   const docRef = await addDoc(collection(this.firestore, "users"), {
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815
+    //   });
+      
+    //   console.log("Document written with ID: ", docRef.id);
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
+
+    // try {
+    //   const docRef = await addDoc(collection(this.firestore, "users"), {
+    //     first: "Alan",
+    //     middle: "Mathison",
+    //     last: "Turing",
+    //     born: 1912
+    //   });
+    
+    //   console.log("Document written with ID: ", docRef.id);
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
+
+//     const querySnapshot = await getDocs(collection(this.firestore, "users"));
+// querySnapshot.forEach((doc) => {
+//   console.log (doc.data(), doc.id);
+// });
+
+
+  }
+   }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  
+  //initalization of firestore
+  providers: [provideRouter(routes),importProvidersFrom([
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyAkqHmFSJUqEBitcthkOVWQLtSciT6XZp4",
+      authDomain: "kanban-nae-shurvat.firebaseapp.com",
+      projectId: "kanban-nae-shurvat",
+      storageBucket: "kanban-nae-shurvat.appspot.com",
+      messagingSenderId: "362116582487",
+      appId: "1:362116582487:web:b42ba2b9b1de6dbe5ee712"
+    })),
+    provideFirestore(() => getFirestore()),
+  ])
+  ]
+  
 };
